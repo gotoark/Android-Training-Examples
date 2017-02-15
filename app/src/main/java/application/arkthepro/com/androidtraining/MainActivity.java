4139f66d151819c3813081b112d710cbff36caf9
RAJESH KUMAR ARUMUGAM <gotoark@gmail.com>
2017-02-15 12:13:06 +0530

    Initial Commit

---
+++ app/src/main/java/application/arkthepro/com/androidtraining/MainActivity.java
@@ -0,0 +1,13 @@
+package application.arkthepro.com.androidtraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
