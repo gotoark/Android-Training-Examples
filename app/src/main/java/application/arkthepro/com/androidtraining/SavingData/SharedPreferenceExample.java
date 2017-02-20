efd50f5971605d81c0632a2e17cb3ba418980c35
RAJESH KUMAR ARUMUGAM <gotoark@gmail.com>
2017-02-20 12:11:20 +0530

    Save Data in File

--- app/src/main/java/application/arkthepro/com/androidtraining/SavingData/SharedPreferenceExample.java
+++ app/src/main/java/application/arkthepro/com/androidtraining/SavingData/SharedPreferenceExample.java
@@ -2,13 +2,27 @@ package application.arkthepro.com.androidtraining.SavingData;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import application.arkthepro.com.androidtraining.R;
 
@@ -28,7 +42,10 @@ public class SharedPreferenceExample extends AppCompatActivity {
         }*/
         final EditText editText=(EditText)findViewById(R.id.edit_text_preference);
         editText.setText(sharedPreferences.getString(NAME_KEY, "no name"));
+        final TextView filecontent=(TextView)findViewById(R.id.filecontent);
         Button add=(Button)findViewById(R.id.btn_addpreference);
+        Button addasfile=(Button)findViewById(R.id.btn_addasfile);
+        Button readFile=(Button)findViewById(R.id.btn_readfile);
 
         editText.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -40,11 +57,67 @@ public class SharedPreferenceExample extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putString(NAME_KEY,editText.getText().toString());
+                editor.putString(NAME_KEY, editText.getText().toString());
                 editor.commit();
-                Toast.makeText(getApplicationContext(),"Shared Preference Updated.Plz Restart the App",Toast.LENGTH_SHORT).show();
+                editText.setText(editText.getText().toString());
+                Toast.makeText(getApplicationContext(), "Shared Preference Updated.Plz Restart the App", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //Creating File
+        String filename="test";
+        final File file=new File(getFilesDir(),filename+".txt");
+
+
+        addasfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //if file not exists then create new File
+                try {
+                    if(!file.exists()){
+                        file.createNewFile();
+                        Log.d("FILE", "File Created Successfully");
+                    }else{
+                        Log.d("FILE", "File Already Exists with name :"+file.getName());
+                    }
+                    FileWriter fw=new FileWriter(file.getAbsoluteFile());
+                    BufferedWriter bw=new BufferedWriter(fw);
+                    String fcontent=editText.getText().toString();
+                    Log.e("FILE", "-----------------------------------Writed: " + fcontent);
+                    bw.write(fcontent);
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("FILE RESULT", "Failed to Create File");
+                }
+
+
+            }
+        });
+        readFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String text="";
+                try {
+                    FileReader fileReader=new FileReader(file.getAbsoluteFile());
+                    BufferedReader br=new BufferedReader(fileReader);
+                    while ((text = br.readLine()) != null) {
+                        stringBuilder.append(text +"\n");
+                        Log.d("STRING BUILDER","-------------------------------ADDED STRING"+stringBuilder.toString());
+                    }
+                 filecontent.setText(stringBuilder.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+
 
     }
 }
